534fca0bfd0af67ae3cc8eef6ef6799c28908f35
Alex Red <alex.redko10@gmail.com>
2026-06-01 10:11:13 +0100

    Add broken clock calculator

--- app/app.py
+++ app/app.py
@@ -327,5 +327,5 @@ def broken_clock_calculate():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5050))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
